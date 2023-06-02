c5334b51b2fe3a1d022a2f96e28c2cbb506934f3
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-06-02 11:58:30 +0100

    Add ex6

---
+++ WriteStuff1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace WriteStuff1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path=GetFullPath(args);
+            Queue<string>strings=new Queue<string>();
+
+            string a=Console.ReadLine();
+            strings.add(a);
+            while (a!=null)
+            {
+                string a=Console.ReadLine();
+                strings.add(a);
+            }
+
+            using (StreamWriter outputfile= new StreamWriter(path))
+            {
+                foreach(string a in strings)
+                {
+                    outputfile.WriteLine(a);
+                }
+            }
+
+
+        }
+    }
+}
