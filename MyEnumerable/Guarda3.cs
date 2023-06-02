1d8398be0a33f80a6ea88947cf6e537baca95332
Vitoria Rodrigues <vvitoria.rodrigues12@gmail.com>
2023-06-02 11:35:31 +0100

    Add IEnumerable

---
+++ MyEnumerable/Guarda3.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyGenericClass
+{
+    public class Guarda3<T>: IEnumerable<T>
+    {
+        private T z;
+        private T x;
+        private T y;
+
+        public Guarda3()
+        {
+            z=default;
+            x=default;
+            y=default;
+        }
+        public T GetItem(int i)
+        {
+            if ((i<0) | (i>2))
+            {
+                throw new IndexOutOfRangeException();
+            }
+        }
+        public void SetItem(int i, T item)
+        {
+            item=i;
+        }
+    }
+}
